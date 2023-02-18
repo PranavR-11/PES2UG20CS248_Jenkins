dee20e9c0146047bd0413df17f85fd3e260426e2
Pranav Rebala <94732433+PranavR-11@users.noreply.github.com>
2023-02-18 12:28:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cour << "Added Webhook\n";
+  cout << "Added Webhook\n";
   return 0;
 }
